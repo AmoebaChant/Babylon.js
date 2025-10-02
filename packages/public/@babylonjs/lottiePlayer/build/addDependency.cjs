628878651d4056c70a2c5203e3391a4132b6ffa5
AmoebaChant <kevbrown@microsoft.com>
2025-10-02 09:40:31 -0700

    add  script to @babylonjs/lottie-player

---
+++ packages/public/@babylonjs/lottiePlayer/build/addDependency.cjs
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+const pkgPath = path.resolve(__dirname, "../package.json");
+let packageText;
+try {
+    packageText = fs.readFileSync(pkgPath, "utf8");
+} catch (err) {
+    console.error(`ERROR: Could not read package.json at ${pkgPath}:`, err.message);
+    process.exit(1);
+}
+
+let packageObject;
+try {
+    packageObject = JSON.parse(packageText);
+} catch (err) {
+    console.error("ERROR: package.json is not valid JSON:", err.message);
+    process.exit(1);
+}
+
+if (!packageObject.dependencies || typeof packageObject.dependencies !== "object") {
+    packageObject.dependencies = {};
+}
+
+packageObject.dependencies["@babylonjs/core-for-lottie-player"] = "file:vendor/babylonjs-core-for-lottie-player-1.0.0.tgz";
+
+try {
+    fs.writeFileSync(pkgPath, JSON.stringify(packageObject, null, 2) + "\n", "utf8");
+} catch (err) {
+    console.error("ERROR: Failed to write updated package.json:", err.message);
+    process.exit(1);
+}
